6d55b8766fbb3bafa0492bfd74ae5bd12fce6a9d
sitthajaid77 <sitthajaid77@gmail.com>
2023-09-19 14:47:05 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -3,40 +3,42 @@ const redis = require("redis");
 dotenv.config({ path: "config.env" });
 
 const opentelemetry = require("@opentelemetry/api");
-const tracer = opentelemetry.trace.getTracer("jung-test-01");
-
-const {
-  RedisInstrumentation,
-} = require("@opentelemetry/instrumentation-redis");
-const { registerInstrumentations } = require("@opentelemetry/instrumentation");
-
-registerInstrumentations({
-  instrumentations: [new RedisInstrumentation()],
-});
+const axios = require("axios");
 
 module.exports.jungtestredisuse = async (event, context, callback) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-
-  const span = tracer.startSpan("do some work");
-  console.log(span);
-  span.setAttribute("foo", "bar");
-  span.end();
-
   try {
-    const client = redis.createClient({
-      url: process.env.REDIS_LOGIN,
-    });
+    const tracer = opentelemetry.trace.getTracer("jungtest01");
 
-    client.on("error", (err) => console.log("Redis Server Error", err));
-    await client.connect();
+    const span = tracer.startSpan("HTTP Request to Google");
+
+    const response = await axios.get("https://www.google.com");
+
+    if (response.status === 200) {
+      const responseBody = response.data;
+
+      span.end();
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(responseBody),
+      };
+    } else {
+      span.setStatus({ code: opentelemetry.SpanStatusCode.ERROR });
+
+      return {
+        statusCode: response.status,
+        body: "HTTP Request Failed",
+      };
+    }
+  } catch (error) {
+    console.error("Error:", error);
+
+    // If there's an error, end the span with an error status
+    span.setStatus({ code: opentelemetry.SpanStatusCode.ERROR });
 
     return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "Log span",
-      }),
+      statusCode: 500,
+      body: "Internal Server Error",
     };
-  } catch (error) {
-    console.log("error = " + err);
   }
 };
