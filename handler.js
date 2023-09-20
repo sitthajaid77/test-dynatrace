d3124a3f49c5e7cd873b4c360197426ea4722920
sitthajaid77 <sitthajaid77@gmail.com>
2023-09-20 11:12:17 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,44 +1,38 @@
 const dotenv = require("dotenv");
 const redis = require("redis");
 dotenv.config({ path: "config.env" });
+const AWS = require("aws-sdk");
 
 const opentelemetry = require("@opentelemetry/api");
+const tracer = opentelemetry.trace.getTracer("jog54314");
 const axios = require("axios");
 
+AWS.config.update({ region: "ap-southeast-1" });
+
 module.exports.jungtestredisuse = async (event, context, callback) => {
+  const span = tracer.startSpan("Start span");
+
+  const lambda = new AWS.Lambda();
+  const params = {
+    FunctionName:
+      "arn:aws:lambda:ap-southeast-1:516577126946:function:dvp-hp-dev-invokeGuestLogin",
+    InvocationType: "RequestResponse", // Use 'RequestResponse' for a synchronous invocation
+    Payload: JSON.stringify({ key: "value" }), // Input data for the target function
+  };
+
+  span.setAttribute("key01", params);
+  span.end();
+
   try {
-    const tracer = opentelemetry.trace.getTracer("jungtest01");
-
-    const span = tracer.startSpan("HTTP Request to Google");
-
-    const response = await axios.get("https://www.google.com");
-
-    if (response.status === 200) {
-      const responseBody = response.data;
-
-      span.end();
-
-      return {
-        statusCode: 200,
-        body: JSON.stringify(responseBody),
-      };
-    } else {
-      span.setStatus({ code: opentelemetry.SpanStatusCode.ERROR });
-
-      return {
-        statusCode: response.status,
-        body: "HTTP Request Failed",
-      };
-    }
+    const response = await lambda.invoke(params).promise();
+    // Handle the response from the target Lambda function
+    console.log("Response from target Lambda:", response.Payload);
   } catch (error) {
-    console.error("Error:", error);
-
-    // If there's an error, end the span with an error status
-    span.setStatus({ code: opentelemetry.SpanStatusCode.ERROR });
-
-    return {
-      statusCode: 500,
-      body: "Internal Server Error",
-    };
+    console.error("Error invoking Lambda:", error);
   }
+
+  return {
+    statusCode: 200,
+    body: "TEST",
+  };
 };
